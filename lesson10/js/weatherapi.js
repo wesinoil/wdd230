d38c9ff8c11345f09b7f83b69a280b6c12ce9452
wesinoil <ver96005@byui.edu>
2022-03-15 11:09:40 -0400

    adding api info

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,21 @@
+const apikey = '1e92c2f0f176d6aa961e863c91cfffdd';
+const cityid = "524901";
+
+
+const apiURL = `http://api.openweathermap.org/data/2.5/forecast?id=${cityid}&appid=${apikey}`;
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+const desc = jsObject.weather[0].description;
+document.querySelector('#current-temp').textContent = jsObject.main.temp;
+document.querySelector('#icon-src').textContent = iconsrc;
+document.querySelector('#weathericon').setAttribute('src', iconsrc);
+document.querySelector('#weathericon').setAttribute('alt', desc);
+document.querySelector('figcaption').textContent = desc;
+
+    });
+
+
