d54e9aa11def23b9c6012a5b3b805fdfe7a81150
wesinoil <ver96005@byui.edu>
2022-03-19 15:13:29 -0400

    directory changes

--- testdirectory/js/directory.js
+++ testdirectory/js/directory.js
@@ -1,39 +1,37 @@
-const requestURL = 'https://wesinoil.github.io/wdd230/testdirectory/data/fldata.json';
-const cards = document.querySelector('.cards');
+const requestURL =
+  "https://wesinoil.github.io/wdd230/testdirectory/data/fldata.json";
+const cards = document.querySelector(".cards");
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const members = jsonObject['members'];
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const members = jsonObject["members"];
 
-   
-  
     function displayMembers(members) {
       // Create elements to add to the document
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let portrait = document.createElement('img');
-    
+      let card = document.createElement("section");
+      let h2 = document.createElement("h2");
+      let address = document.createElement("address");
+      let phone = document.createElement("phone");
+
       // Change the textContent property of the h2 element to contain the members's full name
-      h2.textContent = `${members.name}  ${members.address}`;
-    
+      h2.textContent = `${members.name}`;
+      address.textContent = `${members.address}`;
+      phone.textContent = `${members.phone}`;
       // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
       // portrait.setAttribute('src', members.imageurl);
-      // portrait.setAttribute('alt', 'Portait of ' + members.name + ' ' + members.lastname);
+      // portrait.setAttribute('alt', 'Portait of ' + members.name);
       // portrait.setAttribute('loading', 'lazy');
-    
+
       // Add/append the section(card) with the h2 element
       card.appendChild(h2);
       // card.appendChild(portrait);
-    
+
       // Add/append the existing HTML div with the cards class with the section(card)
-      document.querySelector('div.card').append(card);
+      document.querySelector("div.card").append(card);
     }
     members.forEach(displayMembers);
-
   });
-
-  
