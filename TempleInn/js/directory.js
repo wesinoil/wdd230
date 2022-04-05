b4889ea2704a917d5c3fb57d13e84278e3390e11
wesinoil <ver96005@byui.edu>
2022-04-05 11:52:51 -0400

    data push

--- TempleInn/js/directory.js
+++ TempleInn/js/directory.js
@@ -1,5 +1,5 @@
-const requestURL =
-  "https://wesinoil.github.io/wdd230/testdirectory/data/fldata.json";
+const requestURL = 
+"https://wesinoil.github.io/wdd230/templeinn/data/templedata.json";
 const card = document.querySelector(".card");
 
 fetch(requestURL)
@@ -8,22 +8,22 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject); // temporary checking for valid response and data parsing
-    const members = jsonObject["members"];
+    const temples = jsonObject["temples"];
 
-    function displayMembers(members) {
+    function displayTemples(temples) {
       // Create elements to add to the document
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let address = document.createElement("address");
       let phone = document.createElement("phone");
 
-      // Change the textContent property of the h2 element to contain the members's full name
-      h2.textContent = `${members.name}`;
-      address.textContent = `${members.address}`;
-      phone.textContent = `${members.phone}`;
+      // Change the textContent property of the h2 element to contain the temple name
+      h2.textContent = `${temples.name}`;
+      address.textContent = `${temples.address}`;
+      phone.textContent = `${temples.phone}`;
       // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-      // portrait.setAttribute('src', members.imageurl);
-      // portrait.setAttribute('alt', 'Portait of ' + members.name);
+      // portrait.setAttribute('src', temples.imageurl);
+      // portrait.setAttribute('alt', 'Portait of ' + temples.name);
       // portrait.setAttribute('loading', 'lazy');
 
       // Add/append the section(card) with the h2 element
@@ -35,5 +35,5 @@ fetch(requestURL)
       // Add/append the existing HTML div with the cards class with the section(card)
       document.querySelector("div.card").append(card);
     }
-    members.forEach(displayMembers);
+    temples.forEach(displayTemples);
   });
