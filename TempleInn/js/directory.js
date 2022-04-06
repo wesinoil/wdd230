03cfb9838a0297b91efb06bbc29d8629f663c035
wesinoil <ver96005@byui.edu>
2022-04-06 13:50:39 -0400

    done I think

--- TempleInn/js/directory.js
+++ TempleInn/js/directory.js
@@ -17,20 +17,25 @@ fetch(requestURL)
       let h2 = document.createElement("h2");
       let address = document.createElement("address");
       let phone = document.createElement("phone");
+      let image = document.createElement("imgage");
 
       // Change the textContent property of the h2 element to contain the temple name
       h2.textContent = `${temples.name}`;
       address.textContent = `${temples.address}`;
       phone.textContent = `${temples.phone}`;
+      image.textContent = `${temples.image}`;
       // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
       // portrait.setAttribute('src', temples.imageurl);
       // portrait.setAttribute('alt', 'Portait of ' + temples.name);
       // portrait.setAttribute('loading', 'lazy');
-
+      // image.setAttribute('src', temple.imageurl);
+      // image.setAttribute('alt', 'Image of ' + temple.name);
+      // image.setAttribute('loading');
       // Add/append the section(card) with the h2 element
       card.appendChild(h2);
       card.appendChild(address);
       card.appendChild(phone);
+      card.appendChild(image);
       // card.appendChild(portrait);
 
       // Add/append the existing HTML div with the cards class with the section(card)
